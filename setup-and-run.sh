09380d69c633fad909b03f25aff221b8aad3e883
Alan Montefiore <alan.montefiore@xero.com>
2025-03-30 13:13:34 +1300

    nadakill

--- setup-and-run.sh
+++ setup-and-run.sh
@@ -8,14 +8,6 @@ apt update && apt install -y nano && apt install -y lsof
 git fetch --all
 git reset --hard origin/$(git rev-parse --abbrev-ref HEAD)
 
-if [ -n "$PID" ]; then
-  echo "🔪 Killing process on port $PORT (PID: $PID)..."
-  kill -9 $PID
-  echo "✅ Done."
-else
-  echo "✅ No process running on port $PORT."
-fi
-
 cd ./demo/realtime-img2img
 
 echo "🚀 Launching StreamDiffusion realtime img2img..."
